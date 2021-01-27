5eff2d2332a63f8b2b5be68ea7a5abdab0e04ba7
shivasiddharth <ushivasiddharth@gmail.com>
2021-01-27 12:13:24 +0530

    Update blink-timer-fixed-times.py

--- Micropython/Examples/blink/blink-timer-fixed-times.py
+++ Micropython/Examples/blink/blink-timer-fixed-times.py
@@ -4,6 +4,8 @@ import utime
 # Pin values are GPIO values(numbers) and not physical pin numbers. Please refer the pinout diagram for clarity.
 led = Pin(25, Pin.OUT)
 
+led.value(0)
+
 tim = Timer()
 
 def tick(timer):
@@ -11,16 +13,12 @@ def tick(timer):
     global counter
     led.toggle()
 
-
 # Frequency is inverse of time.Calculate the frequency based on the duration you want the LED to blink.
 tim.init(freq=2, mode=Timer.PERIODIC, callback=tick)
 
 # Alternatively, you can use the period directly giving time value in milliseconds.
 # The following is used to repeat the blinking activity periodically.
 # tim.init(mode=Timer.PERIODIC, period=1000, callback=tick)
-
-# The following is used to carry out the blinking activity once after the set period.
-# tim.init(mode=Timer.ONE_SHOT, period=1000, callback=tick)
 
 for i in range(0,11):
     # Change the value to a higher value to increase the time to stop the timer.
